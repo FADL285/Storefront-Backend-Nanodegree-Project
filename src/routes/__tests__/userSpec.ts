0f5ef43d6bc4994faba84a84d1df2793c9501bfc
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-08-07 00:31:26 +0200

    test: Add User CRUD Endpoint Testing 🧪🧪

--- src/routes/__tests__/userSpec.ts
+++ src/routes/__tests__/userSpec.ts
@@ -54,4 +54,103 @@ describe('User Routes Endpoint Testing', function () {
       expect(response.status).toBe(401)
     })
   })
+
+  describe('User Routes CRUD Endpoints', function () {
+    it('should create a new user & return it', async function () {
+      const response = await request
+        .post('/api/users')
+        .set('Content-Type', 'application/json')
+        .send({
+          email: 'testuser@example.com',
+          username: 'test_user',
+          firstname: 'Test',
+          lastname: 'User',
+          password: 'testPassword'
+        } as IUser)
+
+      expect(response.status).toBe(201)
+      const { email, username } = response.body.data
+      expect(email).toBe('testuser@example.com')
+      expect(username).toBe('test_user')
+    })
+    it('should return an error 409 status code when email or username already exists', async function () {
+      const response = await request
+        .post('/api/users')
+        .set('Content-Type', 'application/json')
+        .send({
+          email: user.email,
+          username: user.username,
+          firstname: 'Test',
+          lastname: 'User',
+          password: 'testPassword'
+        } as IUser)
+
+      expect(response.status).toBe(409)
+    })
+    //  TODO: Create if required params are not provided
+
+    it('should return array of all Users', async function () {
+      const response = await request
+        .get('/api/users')
+        .set('Content-Type', 'application/json')
+      expect(response.status).toBe(200)
+      expect(response.body.data.length).toBe(2)
+    })
+
+    it('should return user data if user already exists', async function () {
+      const response = await request
+        .get(`/api/users/${user.id}`)
+        .set('Content-Type', 'application/json')
+      expect(response.status).toBe(200)
+      expect(response.body.data.email).toBe(user.email)
+    })
+    it('should return 404 status code if id is valid but user does not exist', async function () {
+      const response = await request
+        .get(`/api/users/9cf6e4e5-8508-4a6a-97e2-cb318201db0a`)
+        .set('Content-Type', 'application/json')
+      expect(response.status).toBe(404)
+    })
+    it('should return 400 status code if id is invalid ', async function () {
+      const response = await request
+        .get(`/api/users/9cf6e4e5`)
+        .set('Content-Type', 'application/json')
+      expect(response.status).toBe(400)
+    })
+
+    it('should update the user and return new data', async function () {
+      const response = await request
+        .patch(`/api/users/${user.id}`)
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${authToken}`)
+        .send({
+          email: 'mohamedfadl@admin.com',
+          username: 'mo_fadl'
+        } as IUser)
+
+      expect(response.status).toBe(200)
+      const { email, username } = response.body.data
+      expect(email).toBe('mohamedfadl@admin.com')
+      expect(username).toBe('mo_fadl')
+    })
+    it('should return 401 unauthorized when no valid token is provided', async function () {
+      const response = await request
+        .patch(`/api/users/${user.id}`)
+        .set('Content-Type', 'application/json')
+        .send({
+          username: 'fadl285'
+        } as IUser)
+      expect(response.status).toBe(401)
+    })
+
+    it('should delete the user and return new data', async function () {
+      const response = await request
+        .delete(`/api/users/${user.id}`)
+        .set('Content-Type', 'application/json')
+        .set('Authorization', `Bearer ${authToken}`)
+
+      expect(response.status).toBe(200)
+      const { id } = response.body.data
+      expect(id).toBe(user.id)
+    })
+  })
 })
