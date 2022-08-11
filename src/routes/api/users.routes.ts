43f900fbcadf426bf7daff5b339c3af9c1b0da78
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-08-11 07:26:26 +0200

    Refactor: Update users routes 🚀
    get user orders

--- src/routes/api/users.routes.ts
+++ src/routes/api/users.routes.ts
@@ -7,6 +7,10 @@ import {
   deleteUser,
   authenticateUser
 } from '../../controllers/user.controller'
+import {
+  getUserOrders,
+  getUserOrdersByStatus
+} from '../../controllers/order.controller'
 import authenticationMiddleware from '../../middlewares/authentication.middleware'
 import { createUserValidator } from '../../validations/users.validations'
 import { requestValidationMiddleware } from '../../middlewares/request-validation.middleware'
@@ -18,16 +22,26 @@ router
   .route('/')
   .get(authenticationMiddleware, getAllUsers)
   .post(createUserValidator, requestValidationMiddleware, createUser)
+
 router
   .route('/:id')
   .get(authenticationMiddleware, getUser)
   .patch(authenticationMiddleware, updateUser)
   .delete(authenticationMiddleware, deleteUser)
+
 router.post(
   '/authenticate',
   authenticateUserValidator,
   requestValidationMiddleware,
   authenticateUser
+)
+// Get User Orders
+router.get('/:id/orders', authenticationMiddleware, getUserOrders)
+// Get User Orders By STATUS
+router.get(
+  '/:id/orders/statuses/:status',
+  authenticationMiddleware,
+  getUserOrdersByStatus
 )
 
 export default router
