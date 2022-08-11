406f4bc71435fae4470e136f58c825a8b6065acd
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-08-11 16:21:47 +0200

    feat: Update Order Controller With Order Products Middlewares 🌍🚀

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 import { OrderModel } from '../models/order.model'
 import { IOrder, OrderStatus } from '../interfaces/order.interface'
+import { OrderProductModel } from '../models/order-product.model'
 
 export const getAllOrders = async (
   _req: Request,
@@ -126,6 +127,105 @@ export const updateOrder = async (
     const updatedOrder = await OrderModel.edit(Order)
     res.json({
       data: updatedOrder,
+      status: 'success',
+      statusCode: 200
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const getOrderProducts = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const order = await OrderProductModel.index(req.params.id)
+    res.json({
+      data: order,
+      status: 'success',
+      statusCode: 200
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const getOrderProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const order = await OrderProductModel.show({
+      orderId: req.params.id,
+      productId: req.params.prodId
+    })
+    res.json({
+      data: order,
+      status: 'success',
+      statusCode: 200
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const addOrderProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const order = await OrderProductModel.create({
+      orderId: req.params.id,
+      productId: req.body.productId,
+      quantity: req.body.quantity
+    })
+    res.json({
+      data: order,
+      status: 'success',
+      statusCode: 201
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const updateOrderProductQuantity = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const order = await OrderProductModel.edit({
+      orderId: req.params.id,
+      productId: req.params.prodId,
+      quantity: req.body.quantity
+    })
+    res.json({
+      data: order,
+      status: 'success',
+      statusCode: 200
+    })
+  } catch (err) {
+    next(err)
+  }
+}
+
+export const deleteOrderProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const order = await OrderProductModel.delete({
+      orderId: req.params.id,
+      productId: req.params.prodId
+    })
+    res.json({
+      data: order,
       status: 'success',
       statusCode: 200
     })
